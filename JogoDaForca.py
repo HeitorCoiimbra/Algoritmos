d9499550630e7a02dca6259a9e358bc3388b1fb8
HeitorCoiimbra <heitorciff@gmail.com>
2026-05-06 16:45:49 -0300

    Praticamente finalizado e funcionando perfeitamente (por enquanto).
    Talvez um sistema de dificuldade onde pega de diferentes listas? Pode ser uma ideia legal.

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -1,7 +1,20 @@
+from random import *
 continuarRodada = True
 erros = 0
 
-palavras = "a"
+palavras = ["Zky", "Rszyzydkwy", "Kfsky", "Zkcckby", "Qkdy", "Mkmrybby", "Mkck", "Zybdk", "Myjsxrk", "Mknosbk", "Kbwkbsy", 
+            "Vsfby", "Lsmsmvodk", "Mkbby", "Msnkno", "Wobmkny", "Cyv", "Vek", "Ocdbovk", "Xefow", "Mrefk", "Pyqy", "Kqek", 
+            "Mywzedknyb", "Wydy", "Nyebkny", "Myxromswoxdy", "Nocdsxy", "Wkqscdbknk", "Klscwy", "Mkfkvosby", "Zobposdkwoxdo", 
+            "Boxsdoxdo", "Mkbdk",  "Kmkmsk", "Oxobqsk", "Dowzy", "Zkbknyhy", "Wybdo", "Coxdswoxdy", "Sxdoxcsnkno", "Ckxqeo", 
+            "Kvpklody", "Oqyscwy", "Xysdo", "Vklsbsxdy", "Oxsqwk", "Wony", "Nockccywlbkny", "Wowybsk", "Myvyccy", "Xysdo Coboxk", 
+            "Ymevdy", "Csxy", "Dbkxcwsccky", "Bocoxrk", "Oczsbkv",  "Csxqevkb", "Oczobkxmk", "Rszxyco", "Ylbk-Zbswk", "Fkjsy", 
+            "Csvoxmsy", "Pkxdkcwk", "Hknboj", "Wscdobsy", "Bokvsnkno", "Pbkqwoxdy", "Nkny"]
+
+alfabeto = ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+            "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+palavraCodificada = ""
+palavraDecodificada = ""
+
 palavraEscolhida = ""
 palavraEscolhidaLowercase = ""
 palavraEscondida = []
@@ -16,22 +29,47 @@ formaEscolha = int(input("Como deve ser escolhida a palavra? \n1. Sorteio \n2. E
 if formaEscolha > 2 or formaEscolha < 1:
     print("Escolha inválida")
 else:
+
     if formaEscolha == 1:
-        palavraEscolhida = palavras
+        n = randint(0, (len(palavras) - 1))
+        palavraCodificada = palavras[n].upper()
+
+        for i in range(len(palavraCodificada)):
+            if palavraCodificada[i] == " ":
+                palavraEscolhida += " "
+            else:
+
+                if (palavraCodificada[i] == "-"):
+                    palavraEscolhida += "-"
+
+                else:
+                    posicao = alfabeto.index(palavraCodificada[i])
+                    if i == 0 or palavraCodificada[i - 1] == " " or palavraCodificada[i - 1] == "-":
+                        palavraEscolhida += alfabeto[(posicao - 10) % 26]
+                    else:
+                        palavraEscolhida += alfabeto[(posicao - 10) % 26].lower()
+
     else:
         palavraEscolhida = input("Qual palavra deve ser escolhida para a rodada? ")
         palavraEscolhidaLowercase = palavraEscolhida.lower()
     
     for i in range(len(palavraEscolhida)):
-        if palavraEscolhida[i] not in letrasPalavraEscolhida:
-            letrasPalavraEscolhida += palavraEscolhida[i]
+        if palavraEscolhida[i] != " ":
+            if palavraEscolhida[i] not in letrasPalavraEscolhida:
+                letrasPalavraEscolhida += palavraEscolhida[i]
     
     for i in range(len(palavraEscolhida)):
-        palavraEscondida.append("_")
-
-
+        if palavraEscolhida[i] == " ":
+            palavraEscondida.append(" ")
+        else:
+            if palavraEscolhida[i] == "-":
+                palavraEscondida.append("-")
+            else:
+                palavraEscondida.append("_")
+                
+    print("".join(palavraEscondida))
     while continuarRodada:
-        letra = input("\nEscolha uma letra: ")
+        letra = input("\nEscolha uma letra: ").lower()
 
         if len(letra) != 1:
             print("Digite apenas uma letra!")
@@ -44,28 +82,34 @@ else:
                 if letra.lower() in palavraEscolhida.lower():
                     print(f"A palavra tem {letra}")
                     letrasCertas += letra
+
                     for i in range(len(palavraEscolhida)):
                         if palavraEscolhida[i].lower() == letra.lower():
                             palavraEscondida[i] = palavraEscolhida[i]
                     print("".join(palavraEscondida))
+                    print(f"Erros: {erros}")
+                    print(f"Letras erradas: {letrasErradas}")
 
                 else:
-                    print("Errou paizão")
-                    letrasErradas += letra
+                    print("Essa letra não está na palavra")
+                    letrasErradas += letra + " "
                     erros += 1
+                    print("".join(palavraEscondida))
                     if erros >= 6: 
                         continuarRodada = False
                         print(f"Você foi enforcado! A palavra era: {palavraEscolhida}")
                     else:
-                        print("".join(palavraEscondida))
+                        print(f"Erros: {erros}")
+                    print(f"Letras erradas: {letrasErradas}")
         
         
 
         AcertouTodas = True
         for i in range(len(letrasPalavraEscolhida)):
-            if letrasPalavraEscolhida[i].lower() not in letrasCertas.lower():
-                AcertouTodas = False
+            if letrasPalavraEscolhida[i] != " ":
+                if letrasPalavraEscolhida[i].lower() not in letrasCertas.lower():
+                    AcertouTodas = False
         
         if AcertouTodas == True:
             continuarRodada = False
-            print(f"Você achou a palavra! A palavra era: {palavraEscolhida}")
+            print(f"\nVocê achou a palavra! A palavra era: {palavraEscolhida}")
